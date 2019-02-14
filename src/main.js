3c0365c58824e4872c86eb615d563577f4fb99f3
huayidong <hshadows@163.com>
2019-02-14 16:08:17 +0800

    init

--- src/main.js
+++ src/main.js
@@ -1,8 +1,28 @@
 import Vue from 'vue'
+import VueRouter from 'vue-router';
 import App from './App.vue'
+// import Routers from './router.js';
+import iView from 'iview';
+import 'iview/dist/styles/iview.css';
+
+// Vue.use(VueRouter);
+// Vue.use(iView);
+
+
+// const RouterConfig = {
+//   routes: Routers
+// };
+// const router = new VueRouter(RouterConfig);
+
 
 Vue.config.productionTip = false
 
 new Vue({
   render: h => h(App),
 }).$mount('#app')
+
+// new Vue({
+//   el: '#app',
+//   router: router,
+//   render: h => h(App)
+// });
